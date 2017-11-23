5857550cbd9c9118c7c3e28987a9644e400116f7
patels <sandy.patel@pateltechnologies.com>
2017-11-23 10:06:41 -0600

    passing content from views to html

--- personal/views.py
+++ personal/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
 	return render(request, 'personal/home.html')
+
+def contact(request):
+	return render(request, 'personal/basic.html', {'content':['If you would like to reach me, please email.', 'me@gmial.com']})
